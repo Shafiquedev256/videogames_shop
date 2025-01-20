eb0232e7752c7baa569f33da87628daf2ddfc656
shafiq <shafiq@gmail.com>
2025-01-20 16:44:04 +0300

    first commit

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,7 +1,8 @@
 import Catgory_desplay from "../components/categoryDisplay";
 import { Footer } from "../components/footerSection";
 import Hero_section from "../components/herosection";
-
+import img1 from "../assets/hero3.avif";
+import img2 from "../assets/hero2.avif";
 const Home_page = () => {
   return (
     <>
@@ -13,11 +14,7 @@ const Home_page = () => {
             FIRST-PERSON SHOOTER <br />
             <span>(Coming soon!)</span>
           </h1>
-          <img
-            src='/src/assets/hero3.avif'
-            alt=''
-            className=' md:w-4/5 rounded-lg h-[100%]'
-          />
+          <img src={img1} alt='' className=' md:w-4/5 rounded-lg h-[100%]' />
         </div>
         <Catgory_desplay category='Action' />
         <div className=' md:h-[70vh] h-[50vh] flex flex-col  md:mx-4 md:flex-row-reverse justify-center  items-center overflow-hidden'>
@@ -27,7 +24,7 @@ const Home_page = () => {
           </h1>
           <div className='h-[100%] md:w-4/5'>
             <img
-              src='/src/assets/hero2.avif'
+              src={img2}
               alt=''
               className=' w-[100%] object-center rounded-lg h-[100%]'
             />
